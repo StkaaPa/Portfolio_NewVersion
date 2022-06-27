b83cd82d90f0dd05b199a8e6c93383902cee0ee6
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-06-27 19:32:57 +0100

    main.js

--- main.js
+++ main.js
@@ -5,33 +5,34 @@ const showMenu = (toggleId, navId) =>{
 
   if(toggle && nav){
       toggle.addEventListener('click', ()=>{
-          nav.classList.toggle('show-menu')
+          nav.classList.toggle('show')
       })
   }
 }
-showMenu('nav-toggle','nav-menu')
 
-/* ====== ANIMATE ====== */
+showMenu('nav-toggle','nav-menu') 
+
+/*----- ANIMATE -----*/
 // OVERLAY
-gsap.to(".first", 1.5, { delay: 0.5, top: "-100%", ease: Expo.easeInOut });
-gsap.to(".second", 1.5, { delay: 0.7, top: "-100%", ease: Expo.easeInOut });
-gsap.to(".third", 1.5, { delay: 0.9, top: "-100%", ease: Expo.easeInOut });
-
-// IMAGE
-gsap.from('.home__img', {opcaity: 0, duration: 3.3, delay: 2, x: 1000})
+gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
 
 // NAV ITEM
-gsap.from(".nav__logo", {opacity: 0,duration: 3,delay: 3.2,y: 25,ease: "expo.out",});
-gsap.from(".nav__item", {opacity: 0,duration: 3,delay: 3.2,y: 25,ease: "expo.out",stagger: 0.2,});
+gsap.from('.nav__logo', {opacity: 0, duration: 3, delay: 3.2, y: 25,ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25,ease:'expo.out', stagger: .2})
+
+/*
+// IMG
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
 
 // INFORMATION
-gsap.from(".home__information", { opacity: 0, duration: 3, delay: 2.3, y: 25 });
-gsap.from(".anime-text", {opacity: 0,duration: 3,delay: 2.3,y: 25,ease: "expo.out",stagegr: 0.3,});
+gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
+
 
 // SOCIAL
-gsap.from(".home__social-icon", {opacity: 0,duration: 3,delay: 4,y: 25,ease: "expo.out",stagger: 0.2,});
-
-
+gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})*/
 
 /* ===== DARK LIGHT THEME ===== */
 const themeButton = document.getElementById('moon-button')
