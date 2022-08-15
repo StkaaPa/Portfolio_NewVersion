8704f316ab096e3b5c45dcd3c04dbaa4cc5dc5ed
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-08-15 21:48:59 +0100

    update main.js

--- main.js
+++ main.js
@@ -22,6 +22,47 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*-------------------------------------------------------------------------------------------------------------------------- */
+/* ===== QUALIFICATION TABS ===== */
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () =>{
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+})
+
+/*-------------------------------------------------------------------------------------------------------------------------- */
+/* ===== OPEN/CLOSE SKILLS LIST ===== */
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'
+  }
+  if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills)
+})
+
+/*-------------------------------------------------------------------------------------------------------------------------- */
 /* ===== DARK LIGHT THEME ===== */
 const themeButton = document.getElementById('moon-button')
 const darkTheme = 'dark-theme'
@@ -51,25 +92,3 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-
-/*-------------------------------------------------------------------------------------------------------------------------- */
-/* ===== OPEN/CLOSE SKILLS LIST ===== */
-const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('.skills__header');
-
-function toggleSkills(){
-  let itemClass = this.parentNode.className
-
-  for(i = 0; i < skillsContent.length; i++){
-    skillsContent[i].className = 'skills__content skills__close'
-  }
-  if(itemClass === 'skills__content skills__close'){
-    this.parentNode.className = 'skills__content skills__open'
-  }
-}
-
-skillsHeader.forEach((el) => {
-  el.addEventListener('click', toggleSkills)
-})
-
-
