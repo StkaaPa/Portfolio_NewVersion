affcc1638942e610a175fa55e6d49eb72e1c646b
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-09-27 10:55:20 +0100

    update main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,27 @@
-/* ========== SHOW MENU ========== */
+/* ===== SHOW MENU ===== */
 const showMenu = (toggleId, navId, closeId) =>{
-const toggle = document.getElementById(toggleId), 
+  const toggle = document.getElementById(toggleId), 
         nav = document.getElementById(navId), 
         navClose = document.getElementById(closeId)
 
-if(toggle && nav){
+  if(toggle && nav){
     toggle.addEventListener('click', () => {
-    nav.classList.toggle('show')
+      nav.classList.toggle('show')
     })
-}
+  }
 
-if(navClose)
-    {
+  if(navClose){
     navClose.addEventListener('click', () => {
-    nav.classList.remove('show')
+      nav.classList.remove('show')
     })
-    }
+  }
 }
 showMenu('nav-toggle','nav-menu','nav-close')
 
 
-/* ========== ACTIVE AND REMOVE MENU ========== */
+
+/*--------------------------------------------------------------------------------------- */
+/* ===== ACTIVE AND REMOVE MENU ===== */
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
@@ -29,3 +30,80 @@ function linkAction(){
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*--------------------------------------------------------------------------------------- */
+/* ===== QUALIFICATION TABS ===== */
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () =>{
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+});
+
+
+/*-------------------------------------------------------------------------------------- */
+/* ===== OPEN/CLOSE SKILLS LIST ===== */
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'
+  }
+  if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills)
+});
+
+
+
+/*-------------------------------------------------------------------------------------- */
+/* ===== DARK LIGHT THEME ===== */
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+});
+
