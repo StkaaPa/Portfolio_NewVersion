66a8735aa375eb9ecf4dd31e96a3ce5616844864
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-11-03 10:30:37 +0000

    update main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-/* ===== SHOW MENU ===== */
+//* ===== SHOW MENU ===== */
 const navMenu = document.getElementById('nav-menu'),
       navToogle = document.getElementById('nav-toogle'),
       navClose = document.getElementById('nav-close')
 
-      /* Validate if constant exists */
+      //* Validate if constant exists */
 if(navToogle){
   navToogle.addEventListener('click', () => {
     navMenu.classList.add('show-menu')
@@ -17,20 +17,18 @@ if(navClose){
 }
 
 
-/*--------------------------------------------------------------------------------------- */
-/* ===== ACTIVE AND REMOVE MENU ===== */
+//* ===== ACTIVE AND REMOVE MENU ===== */
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
-  //REMOVE MENU ON MOBILE
+  //* REMOVE MENU ON MOBILE
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-/*--------------------------------------------------------------------------------------- */
-/* ===== QUALIFICATION TABS ===== */
+//* ===== QUALIFICATION TABS ===== */
 const tabs = document.querySelectorAll('[data-target]'),
       tabContents = document.querySelectorAll('[data-content]')
 
@@ -51,8 +49,7 @@ tabs.forEach(tab => {
 });
 
 
-/*-------------------------------------------------------------------------------------- */
-/* ===== OPEN/CLOSE SKILLS LIST ===== */
+//* ===== OPEN/CLOSE SKILLS LIST ===== */
 const skillsContent = document.getElementsByClassName('skills__content'),
       skillsHeader = document.querySelectorAll('.skills__header');
 
@@ -72,36 +69,33 @@ skillsHeader.forEach((el) => {
 });
 
 
-
-/*-------------------------------------------------------------------------------------- */
-/* ===== DARK LIGHT THEME ===== */
+/* ===== DARK LIGHT THEME ===== 
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
 
-// Previously selected topic (if user selected)
+//* Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+//* We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
-// We validate if the user previously chose a topic
+//* We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  //! If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
-// Activate / deactivate the theme manually with the button
+//* Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
+    //* Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
+    //! We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 });
-
-
+*/
