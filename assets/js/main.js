7fcb6ea69dab8905c0a7225e0adebaa9b53979de
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-11-09 01:32:04 +0000

    update main

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,6 +49,36 @@ tabs.forEach(tab => {
 });
 
 
+//*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+//*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+  const nav = document.getElementById('header')
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
 //* ===== OPEN/CLOSE SKILLS LIST ===== */
 const skillsContent = document.getElementsByClassName('skills__content'),
       skillsHeader = document.querySelectorAll('.skills__header');
@@ -69,7 +99,7 @@ skillsHeader.forEach((el) => {
 });
 
 
-/* ===== DARK LIGHT THEME ===== 
+/** ===== DARK LIGHT THEME ===== */
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
@@ -98,4 +128,4 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 });
-*/
+
